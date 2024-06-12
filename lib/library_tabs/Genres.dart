f4f1a1564867b1997b14ed8a8394f08648181e59
darlingson <dmakuwila@gmail.com>
2024-06-12 15:33:08 +0200

    added code show list of genres

--- lib/library_tabs/Genres.dart
+++ lib/library_tabs/Genres.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 
 class GenresTab extends StatefulWidget {
-  const GenresTab({super.key, required List jsonData});
+  final List<dynamic> jsonData;
+  const GenresTab({Key? key, required this.jsonData}) : super(key: key);
 
   @override
   State<GenresTab> createState() => _GenresTabState();
 }
 
 class _GenresTabState extends State<GenresTab> {
+  List<String> genresList = [];
+
+  void getGenresFromSonglist() {
+    for (var song in widget.jsonData) {
+      var genre = song['genre'];
+      if (!genresList.any((element) => element == genre)) {
+        genresList.add(genre);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getGenresFromSonglist();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      itemCount: genresList.length,
+      itemBuilder: (context, index) {
+        var genre = genresList[index];
+        return ListTile(
+          leading: Icon(Icons.type_specimen),
+          title: Text(genre),
+          onTap: () {
+            print('Tapped on: $genre');
+          },
+        );
+      },
+    );
   }
 }
